6d4d98855922a9906c6c498879c41367c7b76446
horizon <thefullhorizon@gmail.com>
2020-10-13 21:42:36 +0800

    【数据分析】以拉钩为平台，初步建立对职位的数据分析流程

--- project/scrawler_tieba/test_entrance.py
+++ project/scrawler_tieba/test_entrance.py
@@ -1,11 +1,8 @@
 # -*- coding=utf-8 -*-
 import webbrowser
 from selenium import webdriver
-from tqdm import tqdm
 
-from project.scrawler_tieba.downloader import HtmlDownloader
-from project.scrawler_tieba.output import Output
-from project.scrawler_tieba.parser import Parser
+from project.scrawler_tieba.crawl import Crawler
 
 '''
 
@@ -17,59 +14,7 @@ what implement mian functions:
 Archive at : /Youdao/COM/Python/Web crawler
 '''
 
-
-class Crawler(object):
-
-    def __init__(self):
-        # initialize tools
-        self.loader = HtmlDownloader()
-        self.parser = Parser()
-        self.output = Output()
-
-    def crawl(self, url, end_page):
-
-        """
-        define the scrawled page
-        :param url:
-        :param end_page: -1 表示用户没指定页数，则爬取得全部帖子
-        :return:
-        """
-        post_statistic_infos = []
-        # if end_page == -1:
-        #     end_page = parser.get_last_number() + 1
-        print("")
-        print(">>> Data obtain...")
-        for i in tqdm(range(end_page)):
-            # generate valid url
-            pn = 50 * i
-            complete_url = url + str(pn)
-
-            # download into local
-            html_doc = self.loader.download(complete_url)
-            if html_doc is None:
-                print('Net work exception')
-                return
-            # parse data
-            result = self.parser.parser_zhengda_tieba(html_doc)
-            for item in result:
-                # 数据存入list中
-                post_statistic_infos.append(item)
-
-        # analysis the data
-        print("")
-        print(">>> Data analysis...")
-        max_l = 0
-        max_object = None
-        for item in post_statistic_infos:
-            if max_l < item.number:
-                max_l = item.number
-                max_object = item
-
-        return max_object
-
-
 if __name__ == '__main__':
-
     print('')
     title = 'We are scrawling web is ZZU post website, please enter the end page:'
     url = 'http://tieba.baidu.com/f?kw=%E9%83%91%E5%B7%9E%E5%A4%A7%E5%AD%A6&ie=utf-8&pn='
@@ -79,7 +24,7 @@ if __name__ == '__main__':
 
     print("")
     print("what the most hot theme is " + the_hot_item.title
-          + " and " + str(the_hot_item) + " people take part in , let's access it ")
+          + " and " + str(the_hot_item.number) + " people take part in , let's access it ")
 
     # 使用浏览器直接打开
     # 方式一（recommend）
@@ -95,4 +40,3 @@ if __name__ == '__main__':
     # self.output.save_to_text(file_name, [the_hot_item])
 
     ## output to db(mongodb)
-
